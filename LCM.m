278df36e6a3c4d5b6d20ee845d680c801d59e0b1
Asim Habib <de.asimhabib@gmail.com>
2020-09-26 22:28:38 +0500

    fixed syntax errors

--- LCM.m
+++ LCM.m
@@ -2,12 +2,12 @@ num1=input('Enter a number to find LCM');
 num2=input('Enter second number to find LCM');
 
 if num1>num2
-    lcm=num1
+    lcm=num1;
 else
-    lcm=num2
+    lcm=num2;
 end
 
-while(mod(lcm,num1)!=0 || mod(lcm,num2)!=0)
+while(mod(lcm,num1)~=0 || mod(lcm,num2)~=0)
     lcm=lcm+1;
 end
 
