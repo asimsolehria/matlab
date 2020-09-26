93baf5054c0560169fcf9776f57635d0d54f7b6a
Asim Habib <de.asimhabib@gmail.com>
2020-09-26 22:24:42 +0500

    Wrote the code for computing/finding lcm of two given numbers

--- LCM.m
+++ LCM.m
@@ -1,2 +1,15 @@
 num1=input('Enter a number to find LCM');
 num2=input('Enter second number to find LCM');
+
+if num1>num2
+    lcm=num1
+else
+    lcm=num2
+end
+
+while(mod(lcm,num1)!=0 || mod(lcm,num2)!=0)
+    lcm=lcm+1;
+end
+
+message=[num2str(lcm) ' is the LCM of given two numbers!'];
+disp(message);
