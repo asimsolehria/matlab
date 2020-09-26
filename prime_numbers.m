00bdc43f6ea2b2d2bf0943b6fbb82f3a0b2d3dfa
Asim Habib <de.asimhabib@gmail.com>
2020-09-26 22:06:11 +0500

    Wrote the code for finding prime numbers between provided range by user

--- prime_numbers.m
+++ prime_numbers.m
@@ -1,6 +1,21 @@
 
 isPrime=true;
-
+counter=0;
 lowerLimit=input('Enter lower limit for finding prime numbers');
 upperLimt=input('Enter upper limit for finding prime numbers');
 
+for i=lowerLimit:upperLimt
+    for j=2:i-2
+        if mod(i,j)==0
+            isPrime=false;
+            break
+        else
+            isPrime=true;
+        end
+    end
+    if isPrime==true
+        counter=counter+1;
+        disp(i);
+    end
+end
+message=[num2str(counter)' Prime numbers were found within the given range!']
